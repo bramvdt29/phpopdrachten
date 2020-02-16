2ff971dd7f11161b0efc2574e6b7cbd29ba2dbfe
Bram van der Tuuk <bram.vandertuuk@edu-kw1c.nl>
2020-02-16 14:43:46 +0100

    Opdrachten PHP

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: Bram van der Tuuk
+ * Date: 15-02-2020
+ * Time: 12:06 PM
+ * File: index.php
+ */
+?>
+<body>
+<?php include 'css/style.css'; ?>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                <ul>
+                    <li>
+                        <a href="Hoofdstuk%202/2.1/opdracht2.1.php">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+</body>
+
+
